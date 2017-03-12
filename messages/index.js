5570fd15bfc2e2daedd3e11fb61accee310ded2e
Menashe <menashe.gezelkopy@gmail.com>
2017-03-12 18:39:38 +0200

    version2.0.606

--- messages/index.js
+++ messages/index.js
@@ -1134,9 +1134,9 @@ blobSvc.createBlockBlobFromLocalFile('imagescontainer', 'contentUrl', attachment
 
                         collTickets.update (
                         { "_id": o_ID },
-                        { $set: { 'attachement': msg, 'AttachmentUploadDate':LogTimeStame }},
+                        { $set: { 'attachement': msg, 'AttachmentUploadDate':LogTimeStame }}
                        // { $push: { Files: { $each: [  results.response  ] } } }
-                        { $push: { Files: { $each: [  {thumbnailUrl: thumbnailUrl, contentUrl: contentUrl, "AttachmentUploadDate" :LogTimeStame, "FileType" : "ticketAttachment" } ] } } }
+                      //  { $push: { Files: { $each: [  {thumbnailUrl: thumbnailUrl, contentUrl: contentUrl, "AttachmentUploadDate" :LogTimeStame, "FileType" : "ticketAttachment" } ] } } }
                         )
 
                          session.send("Nice one! Thanks...");
